6c8e0a8ae4fe9057a8d82e74a011f02a23d46c35
Isreal5161 <diatecktechnology@gmail.com>
2026-03-28 18:59:18 +0100

    User model

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import { verifyToken, authorizeRoles } from "../middleware/auth.js";
 
 import cloudinary from "../config/cloudinary.js";
