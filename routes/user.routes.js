2862ce8e41453f06be9b2415fe8ad28273bc2f66
Isreal5161 <diatecktechnology@gmail.com>
2026-03-28 19:40:23 +0100

    model user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getApprovedProducts } from "../controllers/product.controller.js";
 import { verifyToken } from "../middleware/auth.js";
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
